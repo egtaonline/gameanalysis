fed174506b5874f6b8732e762ca9e325a580fac5
Bryce Wiedenbeck <btwied@gmail.com>
2014-06-25 09:06:57 -0400

    extracted functions: full_profile_symmetric() and full_profile_DPR()
    Created a stand-alone function for finding the full-game profile whose payoffs
    determine the DPR payoffs for a given profile and strategy. This reduced code
    complexity slightly in Reductions and made GP learning experiments much easier
    to set up.

--- Reductions.py
+++ Reductions.py
@@ -5,10 +5,14 @@ from RoleSymmetricGame import Game, Profile, PayoffData, is_symmetric
 def hierarchical_reduction(game, players={}):
 	if not players:
 		players = {r : game.players[r] / 2 for r in game.roles}
+	elif len(game.roles) == 1 and isinstance(players, int):
+		players = {game.roles[0]:players}
+	elif isinstance(players, list):
+		players = dict(zip(game.roles, players))
 	HR_game = type(game)(game.roles, players, game.strategies)
 	for reduced_profile in HR_game.allProfiles():
 		try:
-			full_profile = Profile({r:full_game_profile(reduced_profile[r], \
+			full_profile = Profile({r:full_prof_sym(reduced_profile[r], \
 					game.players[r]) for r in game.roles})
 			HR_game.addProfile({r:[PayoffData(s, reduced_profile[r][s], \
 					game.getPayoffData(full_profile, r, s)) for s in \
@@ -18,10 +22,10 @@ def hierarchical_reduction(game, players={}):
 	return HR_game
 
 
-def full_game_profile(HR_profile, N):
+def full_prof_sym(HR_profile, N):
 	"""
 	Returns the symmetric full game profile corresponding to the given
-	symmetric reduced game profile.
+	symmetric reduced game profile under hierarchical reduction.
 
 	In the event that N isn't divisible by n, we first assign by rounding
 	error and break ties in favor of more-played strategies. The final 
@@ -45,10 +49,27 @@ def full_game_profile(HR_profile, N):
 	return full_profile
 
 
+def full_prof_DPR(DPR_profile, role, strat, players):
+	"""
+	Returns the full game profile whose payoff determines that of strat
+	in the reduced game profile.
+	"""
+	full_prof = {}
+	for r in DPR_profile:
+		if r == role:
+			opp_prof = DPR_profile.asDict()[r]
+			opp_prof[strat] -= 1
+			full_prof[r] = full_prof_sym(opp_prof, players[r] - 1)
+			full_prof[r][strat] += 1
+		else:
+			full_prof[r] = full_prof_sym(DPR_prof[r], players[r])
+	return full_prof
+
+
 def deviation_preserving_reduction(game, players={}):
 	if not players:
 		return twins_reduction(game)
-	elif isinstance(players, int):
+	elif len(game.roles) == 1 and isinstance(players, int):
 		players = {game.roles[0]:players}
 	elif isinstance(players, list):
 		players = dict(zip(game.roles, players))
@@ -56,7 +77,7 @@ def deviation_preserving_reduction(game, players={}):
 	#it's often faster to go through all of the full-game profiles
 	DPR_game = type(game)(game.roles, players, game.strategies)
 	if len(game) <= DPR_game.size and is_symmetric(game):
-		reduced_profile_data = {}
+		DPR_prof_data = {}
 		divisible = True
 		for full_prof in game.knownProfiles():
 			try:
@@ -64,13 +85,13 @@ def deviation_preserving_reduction(game, players={}):
 			except NotImplementedError:
 				divisible = False
 				break
-			if rp not in reduced_profile_data:
-				reduced_profile_data[rp] = {r:[] for r in game.roles}
+			if rp not in DPR_prof_data:
+				DPR_prof_data[rp] = {r:[] for r in game.roles}
 			count = rp[role][strat]
 			value = game.getPayoffData(full_prof, role, strat)
-			reduced_profile_data[rp][role].append(PayoffData(strat,count,value))
+			DPR_prof_data[rp][role].append(PayoffData(strat,count,value))
 		if divisible:
-			for prof_data in reduced_profile_data.values():
+			for prof_data in DPR_prof_data.values():
 				valid_profile = True
 				for r,l in prof_data.items():
 					if sum([d.count for d in l]) != players[r]:
@@ -81,27 +102,15 @@ def deviation_preserving_reduction(game, players={}):
 			return DPR_game
 	
 	#it's conceptually simpler to go through all of the reduced-game profiles
-	for reduced_profile in DPR_game.allProfiles():
+	for DPR_prof in DPR_game.allProfiles():
 		try:
 			role_payoffs = {}
 			for r in game.roles:
 				role_payoffs[r] = []
-				for s in reduced_profile[r]:
-					full_profile = {}
-					for d in game.roles:
-						if r == d:
-							opp_prof = reduced_profile.asDict()[d]
-							opp_prof[s] -= 1
-							full_profile[d] = full_game_profile(opp_prof, \
-									game.players[d] - 1)
-							full_profile[d][s] += 1
-						else:
-							full_profile[d] = full_game_profile( \
-									reduced_profile[d], game.players[d])
-					role_payoffs[r].append(PayoffData(s,reduced_profile[r][s],\
-							game.getPayoffData(Profile(full_profile), r, s)))
-#					p = PayoffData(s,reduced_profile[r][s], game.getPayoffData(Profile(full_profile), r, s))
-#					role_payoffs[r].append(p)
+				for s in DPR_prof[r]:
+					full_prof = full_prof_DPR(DPR_prof, r, s, game.players)
+					role_payoffs[r].append(PayoffData(s,DPR_prof[r][s],\
+							game.getPayoffData(Profile(full_prof), r, s)))
 			DPR_game.addProfile(role_payoffs)
 		except KeyError:
 			continue
@@ -150,23 +159,17 @@ def DPR_profiles(game, players={}):
 	"""Returns the profiles from game that contribute to the DPR game."""
 	if not players:
 		players = {r:2 for r in game.roles}
+	elif len(game.roles) == 1 and isinstance(players, int):
+		players = {game.roles[0]:players}
+	elif isinstance(players, list):
+		players = dict(zip(game.roles, players))
 	DPR_game = Game(game.roles, players, game.strategies)
 	profiles = []
-	for reduced_profile in DPR_game.allProfiles():
-		for role in game.roles:
-			for s in reduced_profile[role]:
-				full_profile = {}
-				for r in game.roles:
-					if r == role:
-						opp_prof = reduced_profile.asDict()[r]
-						opp_prof[s] -= 1
-						full_profile[r] = full_game_profile(opp_prof, \
-								game.players[r] - 1)
-						full_profile[r][s] += 1
-					else:
-						full_profile[r] = full_game_profile(reduced_profile[\
-								r], game.players[r])
-				profiles.append(Profile(full_profile))
+	for DPR_prof in DPR_game.allProfiles():
+		for r in game.roles:
+			for s in DPR_prof[r]:
+				full_prof = full_prof_DPR(DPR_prof, r, s, game.players)
+				profiles.append(Profile(full_prof))
 	return profiles
 
 
