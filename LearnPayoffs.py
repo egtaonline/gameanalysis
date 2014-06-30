706b85104e8fdfbb705a25a9d1634418a738b9e8
Bryce Wiedenbeck <btwied@gmail.com>
2014-06-30 13:57:05 -0400

    fixing my own stupidity

--- LearnPayoffs.py
+++ LearnPayoffs.py
@@ -6,31 +6,22 @@ from sys import argv
 from Reductions import DPR_profiles, full_prof_DPR, DPR
 import RoleSymmetricGame as RSG
 from Nash import mixed_nash
+from BasicFunctions import average
 
-def GP_learn(game, players, var=None):
+def GP_learn(game, var_thresh=10):
 	"""
-	Create a GP regression game with the specified number of players.
-
-	The returned game is constructed using deviation-preserving reduction
-	after estimating the payoffs for the relevant profiles using Gaussian
-	process regression.
+	Create a GP regression for each role and strategy.
 
 	Parameters:
 	game:		RoleSymmetricGame.SampleGame object with enough data to
 				estimate payoff functions.
-	players:	number of players per role in the output game; if symmetric
-				players can be an int, otherwise it must map roles to ints.
-	var:		variance estimate passed to the GP through the nugget parameter.
+	var_thresh:	minimum observations before a profile's variance is estimated.
 	"""
-	if len(game.roles) == 1 and isinstance(players, int):
-		players = {game.roles[0]:players}
-	elif isinstance(players, list):
-		players = dict(zip(game.roles, players))
-
 	x = {r:{s:[] for s in game.strategies[r]} for r in game.roles}
 	y = {r:{s:[] for s in game.strategies[r]} for r in game.roles}
 	gps = {r:{s:None for s in game.strategies[r]} for r in game.roles}
 
+	var = []
 	for p in range(len(game)):
 		c = prof2vec(game, game.counts[p])
 		for r,role in enumerate(game.roles):
@@ -38,6 +29,8 @@ def GP_learn(game, players, var=None):
 				if game.counts[p][r][s] > 0:
 					try: #try will work on RSG.SampleGame
 						samples = game.sample_values[p][r,s]
+						if len(samples) >= var_thresh:
+							var.append(np.var(samples))
 						for i in range(len(samples)):
 							x[role][strat].append(c + \
 									np.random.normal(0,1e-8,c.shape))
@@ -46,14 +39,28 @@ def GP_learn(game, players, var=None):
 					except AttributeError: #except will work on RSG.Game
 						x[role][strat].append(c)
 						y[role][strat].append(game.values[p][r,s])
-	if var == None:
-		var = 1 #TODO: actually estimate the noise variance
+	var = average(var)
 
 	for role in game.roles:
 		for strat in game.strategies[role]:
-			gp = GP(storage_mode='light', normalize=False, nugget=var)
+			gp = GP(storage_mode='light', normalize=False, nugget=var, \
+					random_start=10)
 			gp.fit(x[role][strat], y[role][strat])
 			gps[role][strat] = gp
+	return gps
+
+
+def GP_DPR(game, players, gps=None):
+	"""
+	Estimate a DPR game from GP regression models.
+	"""
+	if len(game.roles) == 1 and isinstance(players, int):
+		players = {game.roles[0]:players}
+	elif isinstance(players, list):
+		players = dict(zip(game.roles, players))
+
+	if gps == None:
+		gps = GP_learn(game)
 
 	learned_game = RSG.Game(game.roles, players, game.strategies)
 	for prof in learned_game.allProfiles():
@@ -68,7 +75,10 @@ def GP_learn(game, players, var=None):
 		learned_game.addProfile(role_payoffs)
 
 	return learned_game
-	
+
+
+
+
 
 def prof2vec(game, prof):
 	"""
@@ -92,7 +102,7 @@ def main(experiments):
 	samples = 20
 	print "trial, reduction regret, learning regret"
 	for j in range(experiments):
-		leg = local_effect_AGG(40,5,2,3,100)
+		leg = local_effect_AGG(41,5,2,3,100)
 
 		fg_reduce = RSG.SampleGame(["All"], {"All":leg.players}, \
 									{"All":leg.strategies})
@@ -116,18 +126,15 @@ def main(experiments):
 		var = []
 		for prof,count in random_profiles.iteritems():
 			values = leg.sample(prof["All"], count)
-			if count >= samples:
-				for s,v in values.iteritems():
-					var.append(np.var(v))
 			fg_learn.addProfile({"All":[RSG.PayoffData(s,c,values[s]) for \
 										s,c in prof["All"].iteritems()]})
-		var = sum(var) / float(len(var))
-		rg_learn = GP_learn(fg_learn, players, var)
+		rg_learn = GP_DPR(fg_learn, players)
 
 		NE_reduce = mixed_nash(rg_reduce, at_least_one = True)[0][0]
 		NE_learn = mixed_nash(rg_learn, at_least_one = True)[0][0]
 
-		print j, leg.regret(NE_reduce), leg.regret(NE_learn)
+		print str(j) +", "+ str(leg.regret(NE_reduce)) +", "+ \
+							str(leg.regret(NE_learn))
 
 
 
