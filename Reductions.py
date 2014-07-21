9d1671ba885058839e10a8f259b6d0a8298fca67
Bryce Wiedenbeck <btwied@gmail.com>
2014-07-21 14:16:03 -0400

    cleaning up my own mess

--- Reductions.py
+++ Reductions.py
@@ -65,7 +65,7 @@ def full_prof_DPR(DPR_profile, role, strat, players):
 			full_prof[r] = full_prof_sym(opp_prof, players[r] - 1)
 			full_prof[r][strat] += 1
 		else:
-			full_prof[r] = full_prof_sym(full_prof[r], players[r])
+			full_prof[r] = full_prof_sym(DPR_profile[r], players[r])
 	return Profile(full_prof)
 
 
