d53af6940403ac784f2ce49444231b84819b784d
Bryce Wiedenbeck <btwied@gmail.com>
2015-03-21 18:00:05 -0400

    GP_EVs

---
+++ GP_EVs.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python2.7
+
+from os.path import exists
+
+import GameIO
+
+def parse_args():
+	parser = GameIO.io_parser()
+	parser.add_argument("mix_file", type=str, help="File with mixtures for "+
+			"which expected values should be calculated.")
+	parser.add_argument("EVs_file", type=str, help="File to which output "+
+			"should be written. Can contain partial output.")
+	return parser.parse_args()
+
+def main():
+	args = parse_args()
+	game = args.input
+	mixtures = GameIO.read(args.mix_file)
+	if exists(args.EVs_file):
+		EVs = GameIO.read(args.EVs_file)
+	else:
+		EVs = []
+	for m in mixtures:
+		EVs.append(game.expectedValues(game.toArray(m)))
+		with open(args.EVs_file,"w") as f:
+			f.write(GameIO.to_JSON_str(EVs))
+
+if __name__ == "__main__":
+	main()
