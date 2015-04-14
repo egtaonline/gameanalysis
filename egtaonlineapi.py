27e233be3e5023fccc1a59b4e235cc920995e6a7
Erik Brinkman <ebrink@umich.edu>
2015-04-14 15:00:39 -0400

    Added potential fix for game corruption, and cleaned up to flake8 standards

--- egtaonlineapi.py
+++ egtaonlineapi.py
@@ -9,6 +9,7 @@ import analysis
 import utils
 import containers
 
+
 def _encode_data(data):
     '''Takes data in nested dictionary form, and converts it for egta
 
@@ -52,12 +53,13 @@ class scheduler(dict):
                 if req['current_count'] < req['requirement']}
 
     def are_profiles_still_active(self, profiles):
-        '''Returns true if any of the profile ids in profiles are still active'''
+        '''Returns true if any of the profile ids in profiles are active'''
         profiles = set(profiles)
         # Update info
         update = self._api.get_scheduler(self.scheduler_id, verbose=True)
         for req in update['scheduling_requirements'] or ():
-            if req['profile_id'] in profiles and req['current_count'] < req['requirement']:
+            if (req['profile_id'] in profiles
+                    and req['current_count'] < req['requirement']):
                 return True
         return False
 
@@ -114,6 +116,7 @@ class scheduler(dict):
         '''
         self._api.delete_generic_scheduler(self.scheduler_id)
 
+
 class egtaonline(object):
     '''Class to wrap egtaonline api'''
     def __init__(self, auth_token, domain='egtaonline.eecs.umich.edu',
@@ -150,7 +153,8 @@ class egtaonline(object):
         '''
         if isinstance(scheduler_name, int):
             data = {'granularity': 'with_requirements'} if verbose else {}
-            resp = self.request('get', 'schedulers/%d.json' % scheduler_name, data)
+            resp = self.request('get', 'schedulers/%d.json' % scheduler_name,
+                                data)
             resp.raise_for_status()
             return scheduler(json.loads(resp.text), _api=self)
 
@@ -160,26 +164,31 @@ class egtaonline(object):
             return self.get_scheduler(named.scheduler_id, verbose=True)
         return named
 
-    def create_generic_scheduler(self, simulator_id, name, active, process_memory, size,
-                                 time_per_observation, observations_per_simulation,
-                                 nodes, default_observation_requirement, configuration):
+    def create_generic_scheduler(self, simulator_id, name, active,
+                                 process_memory, size, time_per_observation,
+                                 observations_per_simulation, nodes,
+                                 default_observation_requirement,
+                                 configuration):
         # pylint: disable=too-many-arguments
         '''Creates a generic scheduler and returns it's id
 
-        simulator_id   - The id of the simulator for which you wish to make a scheduler.
+        simulator_id   - The id of the simulator for which you wish to make a
+                         scheduler.
         name           - The name for the scheduler.
-        active         - True or false, specifying whether the scheduler is initially
-                         active.
+        active         - True or false, specifying whether the scheduler is
+                         initially active.
         process_memory - The amount of memory in MB that your simulations need.
         size           - The number of players for the scheduler.
-        time_per_observation - The time you require to take a single observation.
-        observations_per_simulation - The number of observations to take per simulation
-                         run.
-        nodes          - The number of nodes required to run one of your simulations.
-        default_observation_requirement - The number of observations to take of a
-                         profile in the absence of a specific request.
-        configuration  - A dictionary representation that sets all the run-time parameters
-                         for this scheduler.
+        time_per_observation - The time you require to take a single
+                         observation.
+        observations_per_simulation - The number of observations to take per
+                         simulation run.
+        nodes          - The number of nodes required to run one of your
+                         simulations.
+        default_observation_requirement - The number of observations to take
+                         of a profile in the absence of a specific request.
+        configuration  - A dictionary representation that sets all the
+                         run-time parameters for this scheduler.
 
         '''
         resp = self.request('post', 'generic_schedulers',
@@ -190,9 +199,10 @@ class egtaonline(object):
                                 'process_memory': process_memory,
                                 'size': size,
                                 'time_per_observation': time_per_observation,
-                                'observations_per_simulation': observations_per_simulation,
+                                'observations_per_simulation':
+                                observations_per_simulation,
                                 'nodes': nodes,
-                                'default_observation_requirement': \
+                                'default_observation_requirement':
                                 default_observation_requirement,
                                 'configuration': configuration
                             }})
@@ -211,20 +221,22 @@ class egtaonline(object):
 
     def generic_scheduler_add_role(self, scheduler_id, role, count):
         '''Add a role with specific count to the scheduler'''
-        resp = self.request('post', 'generic_schedulers/%d/add_role.json' % scheduler_id,
-                            data={'role': role, 'count': count})
+        resp = self.request(
+            'post', 'generic_schedulers/%d/add_role.json' % scheduler_id,
+            data={'role': role, 'count': count})
         resp.raise_for_status()
 
     def generic_scheduler_remove_role(self, scheduler_id, role):
         '''Remove a role from the scheduler'''
-        resp = self.request('post', 'generic_schedulers/%d/remove_role.json' % \
-                            scheduler_id, data={'role': role})
+        resp = self.request(
+            'post', 'generic_schedulers/%d/remove_role.json' % scheduler_id,
+            data={'role': role})
         resp.raise_for_status()
 
     def delete_generic_scheduler(self, scheduler_id):
         '''Delete a generic scheduler'''
-        resp = self.request('delete', 'generic_schedulers/%d.json' % \
-                            scheduler_id)
+        resp = self.request('delete',
+                            'generic_schedulers/%d.json' % scheduler_id)
         resp.raise_for_status()
 
     def get_simulators(self):
@@ -268,8 +280,8 @@ class egtaonline(object):
         granularity can be one of:
 
         structure    - returns the game information but no profile information.
-        summary      - returns the game information and profiles with aggregated
-                       payoffs.
+        summary      - returns the game information and profiles with
+                       aggregated payoffs.
         observations - returns the game information and profiles with data
                        aggregated at the observation level.
         full         - returns the game information and profiles with complete
@@ -279,13 +291,11 @@ class egtaonline(object):
         if isinstance(game_name, int):
             # Int implies we have a game id
             #
-            # This call is a little funny, because a path is returned instead of
-            # the data, so a second request has to be made to actually get the data
-            resp = self.request('get', 'games/%d.json' % game_name,
-                                data={'granularity': granularity})
-            resp.raise_for_status()
-            url = 'http://%s%s' % (self.domain, resp.text[resp.text.find('/public/') + 7:])
-            resp = requests.get(url)
+            # This call breaks convention because the api is broken, so we use
+            # a different api.
+            resp = requests.get(
+                'http://egtaonline.eecs.umich.edu/games/%d.json' % game_name,
+                data={'granularity': granularity, 'auth_token': self.auth})
             resp.raise_for_status()
             return (json.loads(json.loads(resp.text))
                     if granularity == 'structure'
@@ -295,7 +305,6 @@ class egtaonline(object):
         if granularity == 'structure':
             return named
         return self.get_game(named['id'], granularity=granularity)
-
 
     def get_profile(self, profile_id):
         '''Get a profile given it's id
@@ -350,7 +359,8 @@ class egtaonline(object):
         else:
             # Iterates through all scheduled profiles for a match, and removes
             # one if found
-            for prof in self.get_scheduler(scheduler_id, True)['scheduling_requirements']:
+            for prof in self.get_scheduler(
+                    scheduler_id, True)['scheduling_requirements']:
                 prof_id = prof['profile_id']
                 symgrps = self.get_profile(prof_id).symmetry_groups
                 if profile_desc == analysis.profile(symgrps):
@@ -359,6 +369,6 @@ class egtaonline(object):
 
     def remove_all_profiles(self, scheduler_id):
         '''Removes all profiles from a scheduler'''
-        for prof in self.get_scheduler(scheduler_id, True)['scheduling_requirements']:
+        for prof in self.get_scheduler(
+                scheduler_id, True)['scheduling_requirements']:
             self.remove_profile(scheduler_id, prof['profile_id'])
-
