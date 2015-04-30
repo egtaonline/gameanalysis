8f9df6ffc896d16bc695d34cf83cb1798eb4e721
Bryce Wiedenbeck <btwied@gmail.com>
2015-04-30 15:54:53 -0400

    GP_Nash

---
+++ GP_Nash.py
@@ -0,0 +1,22 @@
+import GameIO
+import Nash
+
+def parse_args():
+	parser = GameIO.io_parser()
+	parser.add_argument("EVs", choices=["point", "sample", "DPR"])
+	parser.add_argument("--DPR_size", type=int, default=0)
+	args = parser.parse_args()
+	if args.EVs == "DPR":
+		assert args.DPR_size > 0, "DPR requires a value for --DPR_size"
+	return args
+
+def main():
+	args = parse_args()
+	g = args.input
+	g.EVs = args.EVs
+	g.DPR_size = args.DPR_size
+	equilibria = [g.toProfile(eq) for eq in Nash.mixed_nash(g)]
+	print GameIO.to_JSON_str(equilibria)
+
+if __name__ == "__main__":
+	main()
