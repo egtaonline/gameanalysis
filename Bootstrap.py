3260d26c1bc5b59108bcca2cc5fdc0a5039f1531
Varsha Tiruko <vrtiruko@umich.edu>
2015-05-04 16:02:02 -0400

    First implementation of bootstrap

--- Bootstrap.py
+++ Bootstrap.py
@@ -2,13 +2,12 @@
 
 import RandomGames as RG
 
-from GameIO import read, to_JSON_str
+from GameIO import read, to_JSON_str, io_parser
 from Regret import regret
 from Nash import mixed_nash, replicator_dynamics, pure_nash
 from RoleSymmetricGame import SampleGame
 
 from sys import stdin
-from argparse import ArgumentParser
 from random import sample
 from copy import copy, deepcopy
 from functools import partial
@@ -77,8 +76,7 @@ def pre_aggregate(game, count):
 	return agg
 
 
-def bootstrap(game, equilibrium, statistic=regret, method="resample", \
-				method_args=[], points=1000):
+def bootstrap(game, equilibriums, statistic=regret, method="resample", method_args=[], points=1000):
 	"""
 	Returns a bootstrap distribution for the statistic.
 
@@ -89,133 +87,37 @@ def bootstrap(game, equilibrium, statistic=regret, method="resample", \
 	To run a replicator dynamics bootstrap:
 		bootstrap(game, eq, replicator_dynamics)
 	"""
-	boot_dstr = []
-	method = getattr(game, method)
-	for __ in range(points):
-		method(*method_args)
-		boot_dstr.append(statistic(game, equilibrium))
-	game.reset()
-	boot_dstr.sort()
-	return boot_dstr
-
-
-def bootstrap_experiment(base_game_func, noise_model, statistic=regret, \
-		num_games=1000, stdevs=[1.,10.,100.], sample_sizes=[5,10,20, \
-		50,100,200,500], equilibrium_search=mixed_nash, bootstrap_args=[]):
-	results = [{s:{} for s in stdevs} for i in range(num_games)]
-	for i in range(num_games):
-		base_game = base_game_func()
-		if not isinstance(base_game, SampleGame):
-			RG.rescale_payoffs(base_game, 0, 100)
-		for stdev in stdevs:
-			if not isinstance(base_game, SampleGame):
-				sample_game = RG.add_noise(base_game, noise_model, stdev, \
-											sample_sizes[-1])
-			for sample_size in sample_sizes:
-				if isinstance(base_game, SampleGame):
-					subsample_game, base_game = holdout(base_game_func(), \
-														sample_size)
-				else:
-					subsample_game = subsample(sample_game, sample_size)
-				equilibria = equilibrium_search(subsample_game)
-				results[i][stdev][sample_size] = [ \
-					{ \
-						"profile" : eq,
-						"statistic" : statistic(base_game, eq),
-						"bootstrap" : bootstrap(subsample_game, eq, statistic, \
-												*bootstrap_args)
-					} for eq in equilibria]
-	return results
+        boot_lists = [[] for x in xrange(0,len(equilibriums))]
+        method = getattr(game, method)
+        for __ in range(points):
+            method(*method_args)
+            count = 0
+            for eq in equilibriums:
+                boot_lists[count].append(statistic(game, eq))
+                count = count + 1
+        game.reset()
+        conf_intervals = []
+        for boot_list in boot_lists:
+            boot_list.sort()
+            conf_intervals.append(boot_list[950])
+        return conf_intervals
 
 
 def parse_args():
-	parser = ArgumentParser()
-	parser.add_argument("game_func", type=str, default="", choices=sorted([ \
-						f[:-5] for f in RG.game_functions]) + ["None", "single_sample"], help="Specifies "+\
-						"the function generating random base games. If "+\
-						"empty, the script will look for a file with "+\
-						"simulated game data on stdin.")
-	parser.add_argument("noise_func", type=str, default="", choices=sorted([ \
-						f[:-6] for f in RG.noise_functions]) + ["None"], help="Noise "+\
-						"model to perturb sample payoffs around the base "+\
-						"game payoff. May only be empty if first argument "+\
-						"is also empty.")
-	parser.add_argument("-game_args", type=str, nargs="*", default=[], help=\
-						"Arguments to pass to game_func. Usually players "+\
-						"and strategies.")
-	parser.add_argument("-noise_args", type=str, nargs="*", default=[], help=\
-						"Arguments to pass to noise_func. Should not include"+\
-						"noise magnitude or number of samples.")
-	parser.add_argument("-num_games", type=int, default=1000, help="Number "+\
-						"of games to generate per stdev/subsample "+\
-						"combination. Default: 1000")
-	parser.add_argument("-points", type=int, default=1000, help="Number of "+\
-						"bootstrap resamples per equilibrium. Default: 1000")
-	parser.add_argument("-pair", type=str, default="game", choices=["game", \
-						"profile", "payoff"], help="Pairing level to use for "+\
-						"bootstrap resampling. Default: game")
-	parser.add_argument("-agg", type=int, default=0, help="Number of samples "+\
-						"to pre-aggregate. Default: 0")
-	parser.add_argument("-stdevs", type=float, nargs="*", default=\
-						[.1,1.,10.,100.], help="Noise magnitude parameters "+\
-						"passed to the noise model. Default: .1,1,10,100")
-	parser.add_argument("-sample_sizes", type=int, nargs="*", default=\
-						[5,10,20,50,100,200,500], help="Numbers of samples "+\
-						"per profile at which to test the bootstrap. "+\
-						"Default: 5 10 20 50 100 200 500")
-	parser.add_argument("--single", action="store_true", help="Set to use "+\
-						"the single_sample_regret function.")
-	parser.add_argument("--rd", action="store_true", help="Set to compute "+\
-						"bootstrap distributions of equilibrium movement by "+\
-						"replicator dynamics.")
-	parser.add_argument("--pure", action="store_true", help="Find an compute "+\
-						"bootstrap regret distributions for pure-strategy "+\
-						"(rather than role-symmetric mixed-strategy) Nash "+\
-						"equilibria.")
-	args = parser.parse_args()
-
-	if args.game_func == "None":
-		game = read(stdin)
-		args.game_func = lambda: game
-		args.noise_func = lambda s,c: subsample(game, c)
-	else:
-		if args.game_func == "single_sample":
-			game = read(stdin)
-			args.game_func = lambda: game.singleSample()
-		else:
-			game_args = []
-			for a in args.game_args:
-				try:
-					game_args.append(int(a))
-				except ValueError:
-					game_args.append(float(a))
-			args.game_func = partial(getattr(RG, args.game_func + "_game"), \
-										*game_args)
-		noise_args = []
-		for a in args.noise_args:
-			try:
-				noise_args.append(int(a))
-			except ValueError:
-				noise_args.append(float(a))
-		noise_func = getattr(RG, args.noise_func + "_noise")
-		args.noise_func = lambda s,c: noise_func(s,c, *noise_args)
-
-	if args.agg > 0:
-		args.noise_func = lambda s,c: pre_aggregate(args.noise_func(s,c), \
-											args.agg)
-	assert not (args.rd and args.pure), "Must use mixed_nash for rd bootstrap"
-	args.bootstrap_args = ["resample" if not args.single else "singleSample", \
-						[args.pair] if not args.single else [], args.points]
-	return args
+    parser = io_parser()
+    parser.add_argument("profiles", type=str, help="File with profiles from"+\
+        " input games for which confidence intervals should be calculated.")
+    return parser.parse_args()
 
 
 def main():
-	args = parse_args()
-	results = bootstrap_experiment(args.game_func, args.noise_func, \
-					replicator_dynamics if args.rd else regret, \
-					args.num_games, args.stdevs, args.sample_sizes, \
-					pure_nash if args.pure else mixed_nash, args.bootstrap_args)
-	print to_JSON_str(results, indent=None)
+        args = parse_args()
+        game = args.input
+        profiles = read(args.profiles)
+        if not isinstance(profiles, list):
+            profiles = [profiles]
+        results = bootstrap(game,profiles)
+        print to_JSON_str(results, indent=None)
 
 
 if __name__ == "__main__":
