2b83ca1851019fce86fa07a363674f516af6b9d7
Bryce Wiedenbeck <btwied@gmail.com>
2015-05-09 16:02:29 -0400

    batch

--- DPR_Nash.py
+++ DPR_Nash.py
@@ -3,19 +3,25 @@
 import GameIO
 import Nash
 from dpr import DPR
+from argparse import ArgumentParser
+from os.path import join
 
 def parse_args():
-	parser = GameIO.io_parser()
+	parser = ArgumenParser()
 	parser.add_argument("players", type=int)
-	parser.add_argument("--one", action="store_true")
+	parser.add_argument("folder", type=str)
 	return parser.parse_args()
 
 def main():
 	args = parse_args()
-	g = DPR(args.input, args.players)
-	equilibria = [g.toProfile(eq) for eq in Nash.mixed_nash(g, \
-					at_least_one=args.one)]
-	print GameIO.to_JSON_str(equilibria)
+	for i in range(100):
+		game_file = join(args.folder, str(i)+".json")
+		game = DPR(GameIO.read(game_file), args.players)
+		equilibria = [g.toProfile(eq) for eq in Nash.mixed_nash(g, \
+												at_least_one=True)]
+		eq_file = join(args.folder, str(i)+"_DPR-EQ.json")
+		with open(eq_file,"w") as f:
+			f.write(GameIO.to_JSON_str(equilibria))
 
 if __name__ == "__main__":
 	main()
