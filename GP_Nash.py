7ccd8b79a1b0449fbaf95740a84c86e4ea9e83ed
Bryce Wiedenbeck <btwied@gmail.com>
2015-05-09 17:34:55 -0400

    batch

--- GP_Nash.py
+++ GP_Nash.py
@@ -2,12 +2,15 @@
 
 import GameIO
 import Nash
+from argparse import ArgumentParser
+from os.path import join
 
 def parse_args():
-	parser = GameIO.io_parser()
+	parser = ArgumentParser()
+	parser.add_argument("devs", choices=["None", "player", "strat"])
 	parser.add_argument("EVs", choices=["point", "sample", "DPR"])
+	parser.add_argument("folder", type=str)
 	parser.add_argument("--DPR_size", type=int, default=0)
-	parser.add_argument("--one", action="store_true")
 	args = parser.parse_args()
 	if args.EVs == "DPR":
 		assert args.DPR_size > 0, "DPR requires a value for --DPR_size"
@@ -15,12 +18,18 @@ def parse_args():
 
 def main():
 	args = parse_args()
-	g = args.input
-	g.EVs = args.EVs
-	g.DPR_size = args.DPR_size
-	equilibria = [g.toProfile(eq) for eq in Nash.mixed_nash(g, \
-					at_least_one=args.one)]
-	print GameIO.to_JSON_str(equilibria)
+	for i in range(100):
+		game_file = join(args.folder, str(i)+"_GP-"+args.devs[0]+".pkl")
+		game = GameIO.read(game_file)
+		game.EVs = args.EVs
+		game.DPR_size = args.DPR_size
+		equilibria = [game.toProfile(eq) for eq in Nash.mixed_nash(game, \
+													at_least_one=True)]
+		eq_file = join(args.folder, str(i)+"_GP-"+args.devs[0]+\
+						"_EQ-"+args.EVs[0]+(str(args.DPR_size) if \
+						args.EVs == "DPR" else "")+".json")
+		with open(eq_file,"w") as f:
+			f.write(GameIO.to_JSON_str(equilibria))
 
 if __name__ == "__main__":
 	main()
