6879a03f086651ed26b8f8697d29562b471e123f
Bryce Wiedenbeck <btwied@gmail.com>
2015-05-09 20:00:31 -0400

    --no_mean

--- GP_Nash.py
+++ GP_Nash.py
@@ -4,12 +4,14 @@ import GameIO
 import Nash
 from argparse import ArgumentParser
 from os.path import join
+from LearnedModels import ZeroPredictor
 
 def parse_args():
 	parser = ArgumentParser()
-	parser.add_argument("devs", choices=["None", "player", "strat"])
+	parser.add_argument("diffs", choices=["None", "player", "strat"])
 	parser.add_argument("EVs", choices=["point", "sample", "DPR"])
 	parser.add_argument("folder", type=str)
+	parser.add_argument("--no_mean", action="store_true")
 	parser.add_argument("--DPR_size", type=int, default=0)
 	args = parser.parse_args()
 	if args.EVs == "DPR":
@@ -19,15 +21,19 @@ def parse_args():
 def main():
 	args = parse_args()
 	for i in range(100):
-		game_file = join(args.folder, str(i)+"_GP-"+args.devs[0]+".pkl")
+		game_file = join(args.folder, str(i)+"_GP-"+args.diffs[0]+".pkl")
 		game = GameIO.read(game_file)
 		game.EVs = args.EVs
 		game.DPR_size = args.DPR_size
+		if args.no_mean:
+			for role in game.roles:
+				game.GPs[role][None] = ZeroPredictor()
 		equilibria = [game.toProfile(eq) for eq in Nash.mixed_nash(game, \
 													at_least_one=True)]
-		eq_file = join(args.folder, str(i)+"_GP-"+args.devs[0]+\
+		eq_file = join(args.folder, str(i)+"_GP-"+args.diffs[0]+\
 						"_EQ-"+args.EVs[0]+(str(args.DPR_size) if \
-						args.EVs == "DPR" else "")+".json")
+						args.EVs == "DPR" else "")+("d" if args.no_mean else \
+						"")+".json")
 		with open(eq_file,"w") as f:
 			f.write(GameIO.to_JSON_str(equilibria))
 
