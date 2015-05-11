aeb3360e0dafd884ae58e0e583708be681b75f5c
Bryce Wiedenbeck <btwied@gmail.com>
2015-05-11 16:35:46 -0400

    batch

--- LearnPayoffs.py
+++ LearnPayoffs.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python2.7
 
 import cPickle
+from os.path import exists, join
 import GameIO
-
 from LearnedModels import GP_Game
 
 
@@ -12,6 +12,9 @@ def parse_args():
 					"Set to strat or player to learn differences from "
 					"profile-average payoffs; None learns payoffs directly.")
 	parser.add_argument("--CV", action="store_true")
+	parser.add_argument("folder", type=str)
+	parser.add_argument("--start", type=int, default=0)
+	parser.add_argument("--stop", type=int, default=100)
 	args = parser.parse_args()
 	if args.diffs == "None":
 		args.diffs = None
@@ -19,14 +22,17 @@ def parse_args():
 
 
 def main():
-	a = parse_args()
-	g = GP_Game(a.input, a.diffs, a.CV)
-	if a.output != "":
-		sys.stdout.close()
-		with open(a.output, "w") as f:
-			cPickle.dump(g, f)
-	else:
-		print cPickle.dumps(g)
+	args = parse_args()
+	for index in range(args.start, args.stop):
+		out_file = join(args.folder, str(index) + ("_CV-" if args.CV else
+									"_GP-") + args.diffs[0]+".pkl")
+		if exists(out_file):
+			continue
+		with open(join(args.folder, str(index) + ".json")) as f:
+			sample_game = GameIO.read(f)
+		game = GP_Game(sample_game, args.diffs, args.CV)
+		with open(out_file, "w") as f:
+			cPickle.dump(game, f)
 
 
 if __name__ == "__main__":
