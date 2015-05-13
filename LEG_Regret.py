e8a5ab3945410701ef74154936d14845a0096e80
Bryce Wiedenbeck <btwied@gmail.com>
2015-05-13 13:47:37 -0400

    game_type argument

--- LEG_Regret.py
+++ LEG_Regret.py
@@ -11,6 +11,7 @@ import ActionGraphGame as AGG
 
 def parse_args():
 	parser = ArgumentParser()
+	parser.add_argument("game_type", type=str)
 	parser.add_argument("LEG_folder", type=str)
 	parser.add_argument("EQ_folder", type=str)
 	parser.add_argument("--start", type=int, default=0)
@@ -20,23 +21,19 @@ def parse_args():
 def main():
 	args = parse_args()
 	for index in range(args.start, args.stop):
-		with open(join(args.LEG_folder, str(index) + ".json")) as f:
+		eq_file = join(args.EQ_folder, str(index) + game_type + ".json")
+		agg_file = join(args.LEG_folder, str(index) + ".json")
+		out_file = join(args.EQ_folder, str(index) + game_type + "_regret.json")
+		if exists(out_file) or (not exists(eq_fn)):
+			continue
+		with open(agg_file) as f:
 			game = AGG.LEG_to_AGG(json.load(f))
 		empty_game = RSG.Game(["All"], {"All":game.players},
 								{"All":game.strategies})
-		for eq_type in ["_GP-N_EQ-D3","_GP-N_EQ-D5","_GP-N_EQ-p",
-						"_GP-p_EQ-D3","_GP-p_EQ-D5","_GP-p_EQ-p",
-						"_GP-s_EQ-D3","_GP-s_EQ-D5","_GP-s_EQ-D5",
-						"_DPR-EQ"]:
-			regret_fn = join(args.EQ_folder, str(index)+eq_type+"_regret.json")
-			if exists(regret_fn):
-				continue
-			eq_fn = join(args.EQ_folder, str(index)+eq_type+".json")
-			if exists(eq_fn):
-				equilibria = map(empty_game.toArray, GameIO.read(eq_fn))
-				regrets = [game.regret(eq[0]) for eq in equilibria]
-				with open(regret_fn, "w") as rf:
-					json.dump(regrets, rf)
+		equilibria = map(empty_game.toArray, GameIO.read(eq_file))
+		regrets = [game.regret(eq[0]) for eq in equilibria]
+		with open(regret_fn, "w") as f:
+			json.dump(regrets, f)
 
 if __name__ == "__main__":
 	main()
