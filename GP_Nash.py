918bb4737f27384cf9c8e6c340c1699928ff8e06
Bryce Wiedenbeck <btwied@gmail.com>
2015-05-13 09:58:07 -0400

    game_type argument replaces diffs

--- GP_Nash.py
+++ GP_Nash.py
@@ -8,7 +8,8 @@ from LearnedModels import ZeroPredictor
 
 def parse_args():
 	parser = ArgumentParser()
-	parser.add_argument("diffs", choices=["None", "player", "strat"])
+	parser.add_argument("game_type", type=str, help="Input filename, "+
+			"excluding index and extension. Example: CV-N")
 	parser.add_argument("EVs", choices=["point", "sample", "DPR"])
 	parser.add_argument("folder", type=str)
 	parser.add_argument("--no_mean", action="store_true")
@@ -17,14 +18,19 @@ def parse_args():
 	parser.add_argument("--stop", type=int, default=100)
 	args = parser.parse_args()
 	if args.EVs == "DPR":
-		assert args.DPR_size > 0, "DPR requires a value for --DPR_size"
+		assert args.DPR_size > 1, "--DPR_size must be specified"
 	return args
 
 def main():
 	args = parse_args()
 	for i in range(args.start, args.stop):
-		game_file = join(args.folder, str(i)+"_GP-"+args.diffs[0]+".pkl")
-		game = GameIO.read(game_file)
+		in_file = join(args.folder, str(i) +"_"+ args.game_type + ".pkl")
+		out_file = join(args.folder, str(i) +"_"+ args.game_type +"_EQ-"+
+						args.EVs[0] + (str(args.DPR_size) if args.EVs ==
+						"DPR" else "")+("d" if args.no_mean else "")+".json")
+		if exists(out_file):
+			continue
+		game = GameIO.read(in_file)
 		game.EVs = args.EVs
 		game.DPR_size = args.DPR_size
 		if args.no_mean:
@@ -32,11 +38,7 @@ def main():
 				game.GPs[role][None] = ZeroPredictor()
 		equilibria = [game.toProfile(eq) for eq in Nash.mixed_nash(game, \
 													at_least_one=True)]
-		eq_file = join(args.folder, str(i)+"_GP-"+args.diffs[0]+\
-						"_EQ-"+args.EVs[0]+(str(args.DPR_size) if \
-						args.EVs == "DPR" else "")+("d" if args.no_mean else \
-						"")+".json")
-		with open(eq_file,"w") as f:
+		with open(out_file, "w") as f:
 			f.write(GameIO.to_JSON_str(equilibria))
 
 if __name__ == "__main__":
