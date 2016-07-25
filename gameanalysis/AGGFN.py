8c4a2233e21e00e13a2f7bcc7307e2cb9b97b008
Bryce Wiedenbeck <btwied@gmail.com>
2016-07-25 17:49:34 -0400

    AGGFN array implementation basics
    __init__(), min_payoffs(), and deviation_payoffs() are implemented.
    Remaining functions need to be modified, and currently raise errors.

--- gameanalysis/AGGFN.py
+++ gameanalysis/AGGFN.py
@@ -1,68 +1,57 @@
 import numpy as np
 import scipy.special as sps
 from scipy.misc import comb
+from scipy.special import gammaln
 from random import sample
 from itertools import combinations_with_replacement as CwR
 
 from gameanalysis import rsgame
 import sys
 
-tiny = float(np.finfo(np.float64).tiny)
+_TINY = float(np.finfo(np.float64).tiny)
 
-class Sym_AGG_FNA(rsgame.EmptyGame):
-    def __init__(self, players, strategies, function_nodes, action_graph, \
-                 utilities, functions):
-        """
-        Symmetric Action Graph Games with Function Nodes + Additive Structure
-        Inputs:
-            players: count
-            strategies: [strategies]
-            function_nodes: [function nodes]
-            action_graph: {strategy: [neighbor]}
-            utilities: {strategy: np.array[weight]}
-                    For each strategy, store the weight of all its neighbors
-            functions: {function node: list(type of function,(param))}
-        Class Variables:
-            self.neighbors: {node: {neighbor: neighbor's local index}}
-            self.neighbor_index: {node: [if a node is in this neighborhood]}
-            self._dev_reps: np.array[(n-1)C(0),(n-1)C(1),...,(n-1)C(n-1)]
-            self.func_table: store the tabular format of functions
-        """
-        super().__init__({"All":players}, {"All":set(sorted(strategies))})
-        self.function_nodes = function_nodes
-        self.utilities = utilities
-        self.functions = functions
-        self.nodes = sorted(strategies) + function_nodes
-        self.neighbors = {s:{n:i \
-                             for i,n in enumerate(sorted(action_graph[s]))} \
-                                        for s in self.nodes}
-        self.neighbor_index = {s:np.array([n in self.neighbors[s] \
-                                            for n in sorted(self.strategies['All'])]) \
-                                            for s in self.nodes}
-        self.dev_reps = np.array([np.log(comb(players-1,i)) \
-                                        for i in range(players)])
+class Sym_AGG_FNA(rsgame.BaseGame):
+    """Action Graph Game with Function Nodes.
 
-        self.func_table = {p: self.build_func_table(self.functions[p],players) \
-                                for p in self.function_nodes}
+    Represented games are symmetric. Action node utilities have additive structure.
+    Function nodes are contribution-independent. Function nodes have in-edges only
+    from action nodes.
+    """
+    def __init__(self, num_players, num_strategies, action_weights, function_inputs=[],
+                 node_functions=[]):
+        """
+        Parameters
+        ----------
+            num_players
+            num_strategies
+            action_weights: floating point matrix with num_strategies rows and
+                    (num_strategies + |node_functions|) columns. Each entry specifies the
+                    incoming weight in the action graph for the action node (row).
+            function_inputs: boolean matrix with |node_functions| rows and num_strategies
+                    columns. Each entry specifies whether the action node (column) is an
+                    input to the function node (row).
+            node_functions: list of activation functions for each function node. These
+                    functions must work correctly when applied to a vector of inputs.
+        Class Variables
+        ---------------
+            self.action_weights
+            self.function_inputs
+            self.configs
+            self.log_dev_reps
+            self.func_table
+        """
+        super().__init__(num_players, num_strategies)
+        self.action_weights = np.array(action_weights, dtype=float)
+        self.function_inputs = np.array(function_inputs, dtype=bool)
+        self.configs = np.arange(num_players)[:,None]
+        self.dev_reps = comb(num_players - 1, self.configs)
+        self.log_dev_reps = gammaln(num_players) - gammaln(self.configs + 1) - \
+                            gammaln(num_players - self.configs)
+        self.func_table = np.array([f(self.configs[:,0]) for f in
+                                    node_functions], dtype=float)
+        self.num_funcs = self.func_table.shape[0]
+        self.num_nodes = self.num_funcs + self.num_strategies[0]
 
-    def build_func_table(self,func,N):
-        """
-        Input:
-          function: a [func_type, parameters] list
-          N: number of players
-        Output:
-          function values represented as a list
-        """
-        func_type = func[0]
-        param = func[1]
-        if func_type == 'linear':
-            k, c = param
-            return [k*n + c for n in range(N+1)]
-        elif func_type == 'quadratic':
-            a,b,c = param
-            return [a*n**2 + b*n + c for n in range(N+1)]
-        else:
-            raise KeyError("No such function type")
 
     @staticmethod
     def from_json(json_):
@@ -70,6 +59,7 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
         Build a game from the information stored in a dictionary in the json
         format
         """
+        raise NotImplementedError("not yet compatible with array implementation.")
         return Sym_AGG_FNA(json_['players'],
                            json_['strategies'],
                            json_['function_nodes'],
@@ -78,82 +68,46 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
                            json_['functions'])
 
 
-    def min_payoffs(self, as_array=False):
+    def min_payoffs(self):
         """
         Find a lower bound of the payoffs
         """
-        # d is a dictionary that maps a node to its (min,max) values
-        d = {}
-        for node in self.nodes:
-            if node in self.function_nodes:
-                d[node] = (np.min(self.func_table[node]),
-                           np.max(self.func_table[node]))
-            elif node in self.strategies['All']:
-                d[node] = (0, self.players['All'])
+        minima = np.zeros(self.num_strategies[0] + self.num_funcs)
+        minima[-self.num_funcs:] = self.func_table.min(1)
+        minima = minima[:,None].repeat(self.num_strategies[0], 1)
+        minima[self.action_weights <= 0] = 0
 
-        min_payoff = float('inf')
-        for s in self.strategies['All']:
-            min_config = np.zeros(len(self.neighbors[s]))
-            for n,i in self.neighbors[s].items():
-                if self.utilities[s][i] >= 0:
-                    min_config[i] = d[n][0]
-                else:
-                    min_config[i] = d[n][1]
-            min_s = np.dot(np.array(min_config), self.utilities[s])
-            if min_s < min_payoff:
-                min_payoff = min_s
+        maxima = np.empty(self.num_strategies[0] + self.num_funcs)
+        maxima.fill(self.num_players[0])
+        maxima[-self.num_funcs:] = self.func_table.max(1)
+        maxima = maxima[:,None].repeat(self.num_strategies[0], 1)
+        maxima[self.action_weights >= 0] = 0
 
-        if as_array or as_array is None:
-            return np.array([min_payoff])
-        else:
-            return {'All': min_payoff}
+        return ((minima + maxima) * self.action_weights).sum(0).min(keepdims=True)
 
 
-    def deviation_payoffs(self, mix, verify=True, assume_complete=False,
-                          jacobian=False, as_array=False):
-        """
-        Computes the deviation_payoffs
-        EVs: list of payoffs of playing each strategy against the mixture
-        local_mix: the mixture projected to this neighborhood
-        """
-        mix = self.as_mixture(mix, verify=False, as_array=True)
-        EVs = []
+    def deviation_payoffs(self, mix, assume_complete=True, jacobian=False):
+        # TODO To add jacobian support.
+        assert not jacobian, "Sym_AGG_FNA doesn't support jacobian"
+        func_node_probs = mix[:,None].repeat(self.num_funcs, 1)
+        func_node_probs[np.logical_not(self.function_inputs)] = 0
+        func_node_probs = func_node_probs.sum(0)
 
-        p_probs = [sum(mix[self.neighbor_index[p]]) for p in self.function_nodes]
-        probs = np.append(mix, np.array(p_probs))
+        act_conf_probs = np.exp(self.log_dev_reps + np.log(mix+_TINY) * self.configs +
+                                np.log(1-mix+_TINY) * (self.num_players-1-self.configs))
+        func_conf_probs = np.exp(self.log_dev_reps + np.log(func_node_probs + _TINY) *
+                                 self.configs + np.log(1 - func_node_probs + _TINY) *
+                                 (self.num_players - 1 - self.configs))
+        act_EVs = (act_conf_probs * self.configs).sum(0)
+        func_EVs = (func_conf_probs * self.func_table.T).sum(0)
+        return (self.action_weights * np.append(act_EVs, func_EVs)).sum(1)
 
-        for strat in sorted(self.strategies['All']):
-
-            # EV:
-            EV = np.zeros(len(self.neighbors[strat]),float)
-            for s,i in self.neighbors[strat].items():
-                prob = probs[self.nodes.index(s)]
-                if prob + tiny >= 1:
-                    prob = 1 - 2 * tiny
-
-                sigma = np.array([(np.log(prob+tiny) * i + \
-                                  np.log(1-prob+tiny) * (self.players['All']-i-1)) \
-                                  for i in range(self.players['All'])])
-
-                if s in self.strategies['All']:
-                    f = np.array(range(self.players['All']))
-                    if s == strat:
-                        f += 1
-                elif s in self.function_nodes:
-                    if strat in self.neighbors[s]:
-                        f = self.func_table[s][1:]
-                    else:
-                        f = self.func_table[s][:-1]
-
-                EV[i] = np.dot(f, np.exp(self.dev_reps + sigma))
-            EVs.append(np.dot(EV,self.utilities[strat]))
-
-        return np.array(EVs)
 
     def to_json(self):
         """
         Creates a json format of the game for storage
         """
+        raise NotImplementedError("not yet compatible with array implementation.")
         json = {}
         json['players'] = self.players['All']
         json['strategies'] = list(self.strategies['All'])
@@ -174,6 +128,7 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
         w_mean: mean of weights
         w_var: variance of weights
         """
+        raise NotImplementedError("not yet compatible with array implementation.")
         if D_min < 0 or D_min >= num_strats:
             D_min = 0
         if D_max < 0 or D_max >= num_strats:
@@ -221,6 +176,7 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
         Output:
             The payoff
         """
+        raise NotImplementedError("not yet compatible with array implementation.")
         payoff = 0
         for s,i in self.neighbors[strat].items():
             if s in self.strategies['All']:
@@ -240,6 +196,7 @@ class Sym_AGG_FNA(rsgame.EmptyGame):
         game. There will only be one role "All" in the constructed
         rsgame
         """
+        raise NotImplementedError("not yet compatible with array implementation.")
         aprofiles = []
         apayoffs = []
         for profile in CwR(self.strategies['All'],self.players['All']):
