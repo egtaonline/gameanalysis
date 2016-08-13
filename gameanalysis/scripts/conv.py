5dc31a17be1c4751f0f4ceecd64685cd8861b5f7
Erik Brinkman <erik.brinkman@gmail.com>
2016-08-13 10:08:56 -0400

    Allow conv to change game type

--- gameanalysis/scripts/conv.py
+++ gameanalysis/scripts/conv.py
@@ -5,10 +5,18 @@ import sys
 from os import path
 
 from gameanalysis import gameio
+from gameanalysis import rsgame
 
 
 OUTPUT_TYPE = {
     'json': lambda game, ser, out: json.dump(ser.to_json(game), out)
+}
+
+GAME_TYPE = {
+    'identity': lambda g: g,
+    'base': rsgame.BaseGame,
+    'game': rsgame.Game,
+    'sample': rsgame.SampleGame,
 }
 
 PACKAGE = path.splitext(path.basename(sys.modules[__name__].__file__))[0]
@@ -22,6 +30,8 @@ PARSER.add_argument('--input', '-i', metavar='<input-file>', default=sys.stdin,
 PARSER.add_argument('--output', '-o', metavar='<output-file>',
                     default=sys.stdout, type=argparse.FileType('w'),
                     help="""Output file for script. (default: stdout)""")
+PARSER.add_argument('--type', '-t', choices=GAME_TYPE, default='identity',
+                    help="""Output game type. (default: %(default)s)""")
 PARSER.add_argument('--format', '-f', choices=OUTPUT_TYPE, default='json',
                     help="""Output format. (default: %(default)s)""")
 
@@ -29,6 +39,7 @@ PARSER.add_argument('--format', '-f', choices=OUTPUT_TYPE, default='json',
 def main():
     args = PARSER.parse_args()
     game, serial = gameio.read_game(json.load(args.input))
+    game = GAME_TYPE[args.type](game)
     OUTPUT_TYPE[args.format](game, serial, args.output)
     args.output.write('\n')
 
