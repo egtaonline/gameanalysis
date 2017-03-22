548f33530eaf83488f5898ba103f94de8ae39668
Erik Brinkman <erik.brinkman@gmail.com>
2017-03-22 10:01:06 -0400

    Remove unnecessary parameter from ga

--- gameanalysis/script.py
+++ gameanalysis/script.py
@@ -7,12 +7,11 @@ from gameanalysis import scripts
 def create_parser():
     modules = [imp.find_module(name).load_module(name) for imp, name, _
                in pkgutil.iter_modules(scripts.__path__)]
-    parser = argparse.ArgumentParser(prog='ga', description="""Command line
-                                     access to the game analysis toolkit.""")
-    subparsers = parser.add_subparsers(title='commands', dest='command',
-                                       metavar='<command>', help="""The
-                                       commands to execute. Available commands
-                                       are:""")
+    parser = argparse.ArgumentParser(
+        description="""Command line access to the game analysis toolkit.""")
+    subparsers = parser.add_subparsers(
+        title='commands', dest='command', metavar='<command>', help="""The
+        commands to execute. Available commands are:""")
     for module in modules:
         subparser = module.add_parser(subparsers)
         subparser.main = module.main
