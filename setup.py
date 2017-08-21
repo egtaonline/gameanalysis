05e4c453eef7acb6cb78b118e39a3f45295cfc78
Erik Brinkman <erik.brinkman@gmail.com>
2017-08-21 17:05:35 -0400

    Patch bump

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ import setuptools
 
 
 setuptools.setup(
-    name="gameanalysis",
-    version="3.2.0",
-    description="A python module for analyzing sparse and empirical games",
-    url="https://github.com/egtaonline/gameanalysis.git",
-    author="Strategic Reasoning Group",
+    name='gameanalysis',
+    version='3.2.1',
+    description='A python module for analyzing sparse and empirical games',
+    url='https://github.com/egtaonline/gameanalysis.git',
+    author='Strategic Reasoning Group',
     author_email='strategic.reasoning.group@umich.edu',
-    license="Apache 2.0",
-    entry_points={"console_scripts": ["ga=gameanalysis.__main__:main"]},
+    license='Apache 2.0',
+    entry_points={'console_scripts': ['ga=gameanalysis.__main__:main']},
     install_requires=[
-        "numpy~=1.13",
-        "scipy~=0.19",
-        "scikit-learn~=0.18",
+        'numpy~=1.13',
+        'scipy~=0.19',
+        'scikit-learn~=0.18',
     ],
     packages=[
-        "gameanalysis",
-        "gameanalysis.script",
+        'gameanalysis',
+        'gameanalysis.script',
     ],
 )
