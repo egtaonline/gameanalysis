Fix nans in payoff jacobian

gameanalysis/aggfn.py
@@ -126,8 +126,8 @@ class AgfnGame(rsgame.CompleteGame):
             (self.num_roles, self.num_functions) +
             tuple(self.num_role_players + 1),
             float)
-        for i, (num_play, probs, zp) in enumerate(zip(
-                self.num_role_players, role_node_probs, self.zero_prob)):
+        for i, (num_play, probs) in enumerate(zip(self.num_role_players,
+                                                  role_node_probs)):
             role_probs = spt.binom.pmf(
                 np.arange(num_play + 1), num_play, probs)
             dev_role_probs = spt.binom.pmf(
@@ -159,6 +159,9 @@ class AgfnGame(rsgame.CompleteGame):
                          tuple(self.num_role_players + 1), float)
         for i, (num_play, probs, zp) in enumerate(zip(
                 self.num_role_players, role_node_probs, self.zero_prob)):
+            # TODO This zp threshold causes large errors in the jacobian when
+            # we look at sparse mixtures. This should probably be addressed,
+            # but it's unclear how without making this significantly slower.
             configs = np.arange(num_play + 1)
             der = configs / (probs + zp) - configs[::-1] / (1 - probs + zp)
             dev_der = np.insert(configs[:-1] / (probs + zp) - configs[-2::-1] /

gameanalysis/paygame.py
@@ -192,8 +192,11 @@ class Game(rsgame.RsGame):
 
             # Mask out nans
             zp = self.zero_prob.dot(self.num_role_players)
-            devs = np.einsum(
-                'ij,ij->j', probs, np.where(probs > zp, payoffs, 0))
+            # TODO This threshold causes large errors in the jacobian when we
+            # look at sparse mixtures. This should probably be addressed, but
+            # it's unclear how without making this significantly slower.
+            nan_pays = np.where(probs > zp, payoffs, 0)
+            devs = np.einsum('ij,ij->j', probs, nan_pays)
             devs[nan_mask] = np.nan
 
         else:
@@ -212,7 +215,7 @@ class Game(rsgame.RsGame):
             dev_profs = (self._profiles[:, None] -
                          np.eye(self.num_strats, dtype=int))
             dev_jac = np.einsum(
-                'ij,ij,ijk->jk', probs, payoffs, dev_profs) / zmix
+                'ij,ij,ijk->jk', probs, nan_pays, dev_profs) / zmix
             if ignore_incomplete:
                 dev_jac -= (np.einsum('ij,ijk->jk', probs, dev_profs) *
                             devs[:, None] / zmix)