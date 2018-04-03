06a0561cc6561d2f9056e5fc49b6a98f97a98d4c
Erik Brinkman <erik.brinkman@gmail.com>
2018-04-03 16:10:51 -0400

    Prune test and make bitset take size
    - This is an API breaking change

--- gameanalysis/collect.py
+++ gameanalysis/collect.py
@@ -1,11 +1,6 @@
 import bisect
-from collections import abc
 
 import numpy as np
-
-from gameanalysis import utils
-
-# TODO Prune unused collections
 
 
 def mcces(thresh):
@@ -65,159 +60,28 @@ class MinimumConnectedComponentElementSet(object):
             self.__class__.__name__, self._thresh, list(self))
 
 
-@utils.deprecated
-class WeightedSimilaritySet(object):
-    """A set of non-similar elements prioritized by weight
-
-    Allows adding a bunch of weighted elements, and when iterated, only
-    iterates over dissimilar elements with the lowest weights. Adding new
-    elements that are similar to the existing set, but with higher weights
-    won't change the set returned."""
-
-    def __init__(self, is_similar):
-        self._is_similar = is_similar
-        self._i = 0  # Tie breaking
-        self._items = []
-        self._set = []
-        self._computed = True
-
-    def add(self, item, weight):
-        self._computed = False
-        self._set.clear()
-        self._items.append((weight, self._i, item))
-        self._i += 1
-        return self
-
-    def clear(self):
-        self._i = 0
-        self._items.clear()
-        self._set.clear()
-        self._computed = True
-
-    def _satisfy(self):
-        if not self._computed:
-            self._items.sort()
-            for w, _, i in self._items:
-                if all(not self._is_similar(i, j) for j, _ in self._set):
-                    self._set.append((i, w))
-            self._computed = True
-
-    def __len__(self):
-        self._satisfy()
-        return len(self._set)
-
-    def __iter__(self):
-        self._satisfy()
-        return iter(self._set)
-
-    def __repr__(self):
-        self._satisfy()
-        suffix = ('.add(' + ').add('.join('{}, {}'.format(i, w)
-                                          for w, _, i in self._items) + ')'
-                  if self._items else '')
-        return '{}({}){}'.format(self.__class__.__name__,
-                                 self._is_similar.__name__, suffix)
-
-
-class DynamicArray(object):
-    """A object with a backed array that also allows adding data"""
-
-    def __init__(self, item_shape, dtype=None, initial_room=8,
-                 grow_fraction=2):
-        assert grow_fraction > 1
-        if not isinstance(item_shape, abc.Sized):
-            item_shape = (item_shape,)
-        self._data = np.empty((initial_room,) + tuple(item_shape), dtype)
-        self._length = 0
-        self._grow_fraction = 2
-
-    def append(self, array):
-        """Append an array"""
-        array = np.asarray(array, self._data.dtype)
-        if array.shape[1:] == self._data.shape[1:]:
-            # Adding multiple
-            num = array.shape[0]
-            self.ensure_capacity(self._length + num)
-            self._data[self._length:self._length + num] = array
-            self._length += num
-
-        elif array.shape == self._data.shape[1:]:
-            # Adding one
-            self.ensure_capacity(self._length + 1)
-            self._data[self._length] = array
-            self._length += 1
-
-        else:
-            raise ValueError("Invalid shape for append")
-
-    def pop(self, num=None):
-        """Pop one or several arrays"""
-        if num is None:
-            assert self._length > 0, "can't pop from an empty array"
-            self._length -= 1
-            return self._data[self._length].copy()
-
-        else:
-            assert num >= 0 and self._length >= num
-            self._length -= num
-            return self._data[self._length:self._length + num].copy()
-
-    @property
-    def data(self):
-        """A view of all of the data"""
-        return self._data[:self._length]
-
-    def ensure_capacity(self, new_capacity):
-        """Make sure the array has a least new_capacity"""
-        if new_capacity > self._data.shape[0]:
-            growth = round(self._data.shape[0] * self._grow_fraction) + 1
-            new_size = max(growth, new_capacity)
-            new_data = np.empty((new_size,) + self._data.shape[1:],
-                                self._data.dtype)
-            new_data[:self._length] = self._data[:self._length]
-            self._data = new_data
-
-    def compact(self):
-        """Trim underlying storage to hold only valid data"""
-        self._data = self.data.copy()
-        self._length = self._data.shape[0]
-
-    def __iter__(self):
-        return iter(self.data)
-
-    def __len__(self):
-        return self._length
-
-    def __str__(self):
-        return str(self.data)
-
-    def __repr__(self):
-        return repr(self.data)
-
-
-def bitset(iterable=()):
-    bits = BitSet()
+def bitset(dim, iterable=()):
+    bits = BitSet(dim)
     for bit in iterable:
         bits.add(bit)
     return bits
 
 
-# TODO Add a size parameter and add all false
 class BitSet(object):
     """Set of bitmasks
 
     A bitmask is in the set if all of the true bits have been added
-    together. When iterating, all maximal bitsets are returned."""
+    together. When iterating, all maximal bitsets are returned. An empty bitset
+    still contains 0."""
     # This compresses all bitmasks down to the number they are
     # implicitly, and uses bitwise math to replicate the same functions.
 
-    def __init__(self):
-        self._masks = []
+    def __init__(self, dim):
+        self._masks = [0]
+        self._mask = 2 ** np.arange(dim)
 
     def add(self, bitmask):
         bitmask = np.asarray(bitmask, bool)
-        if not self._masks:
-            self._mask = 2 ** np.arange(bitmask.size)
         if bitmask not in self:
             num = bitmask.dot(self._mask)
             self._masks[:] = [m for m in self._masks if m & ~num]
@@ -228,10 +92,9 @@ class BitSet(object):
 
     def clear(self):
         self._masks.clear()
+        self._masks.append(0)
 
     def __contains__(self, bitmask):
-        if not self._masks:
-            return False
         assert bitmask.size == self._mask.size, \
             "can't add bitmasks of different sizes"
         num = bitmask.dot(self._mask)
@@ -242,49 +105,11 @@ class BitSet(object):
 
     def __eq__(self, other):
         return (type(self) is type(other) and
-                (not self._masks and not other._masks) or
-                (self._mask.size == other._mask.size and
-                 frozenset(self._masks) == frozenset(other._masks)))
+                self._mask.size == other._mask.size and
+                frozenset(self._masks) == frozenset(other._masks))
 
     def __bool__(self):
-        return bool(self._masks)
+        return len(self._masks) > 1 or bool(self._masks[0] != 0)
 
     def __repr__(self):
         return '{}({!r})'.format(self.__class__.__name__, self._masks)
-
-
-@utils.deprecated
-class MixtureSet(object):
-    """A set of mixtures
-
-    Elements are only kept if the norm of their difference is greater than the
-    tolerance."""
-
-    def __init__(self, tolerance):
-        self._tol = tolerance ** 2
-        self._mixtures = []
-
-    def add(self, mixture):
-        if mixture not in self:
-            self._mixtures.append(mixture)
-            return True
-        else:
-            return False
-
-    def clear(self):
-        self._mixtures.clear()
-
-    def __contains__(self, mix):
-        return any(np.dot(m - mix, m - mix) <= self._tol for m in self)
-
-    def __bool__(self):
-        return bool(self._mixtures)
-
-    def __len__(self):
-        return len(self._mixtures)
-
-    def __iter__(self):
-        return iter(self._mixtures)
-
-    def __repr__(self):
-        return '{}({!r})'.format(self.__class__.__name__, self._mixtures)
--- test/test_collect.py
+++ test/test_collect.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pytest
 
 from gameanalysis import collect
 from gameanalysis import utils
@@ -29,50 +28,8 @@ def test_connected_component():
     assert [((0,), 0), ((3,), 2)] == list(simset)
 
 
-def test_weighted_similarity():
-    simset = collect.WeightedSimilaritySet(lambda a, b: abs(a - b) <= 1)
-    simset.add(1, 0)
-    simset.add(2, 1)
-    simset.add(0, .5)
-    assert len(simset) == 1
-    assert len(simset) == 1  # computed not
-    assert [(1, 0)] == list(simset)
-    repr(simset)
-
-    simset.clear()
-    simset.add(0, .5)
-    simset.add(2, 1)
-    simset.add(1, 0)
-    assert [(1, 0)] == list(simset)
-
-
-def test_dynamic_array():
-    darray = collect.DynamicArray(3, dtype=int)
-    str(darray)
-    repr(darray)
-    darray.ensure_capacity(100)
-    darray.append([0, 1, 2])
-    darray.append([[3, 4, 5], [6, 7, 8]])
-    assert len(darray) == 3
-    assert np.all(darray.data == [[0, 1, 2], [3, 4, 5], [6, 7, 8]])
-    assert all(np.all(x == y) for x, y
-               in zip(darray, [[0, 1, 2], [3, 4, 5], [6, 7, 8]]))
-    assert np.all(darray.pop() == [6, 7, 8])
-    darray.compact()
-    assert np.all(darray.pop(2) == [[0, 1, 2], [3, 4, 5]])
-
-    darray = collect.DynamicArray([3], dtype=int)
-    darray.append([0, 1, 2])
-    assert len(darray) == 1
-
-
-def test_dynamic_array_failure():
-    with pytest.raises(ValueError):
-        collect.DynamicArray(3, dtype=int).append([0, 0])
-
-
-def test_bit_set():
-    bitset = collect.bitset()
+def test_bitset():
+    bitset = collect.bitset(5)
     assert not bitset
     a = np.array([0, 1, 1, 1, 0], bool)
     b = np.array([1, 0, 0, 1, 1], bool)
@@ -89,22 +46,10 @@ def test_bit_set():
     actual = frozenset(map(utils.hash_array, bitset))
     assert expected == actual
     assert bitset
-    assert bitset == collect.bitset([a, b])
+    assert bitset == collect.bitset(5, [a, b])
 
     bitset.clear()
     assert not bitset
-    assert repr(bitset) == 'BitSet([])'
-    assert list(bitset) == []
+    assert repr(bitset) == 'BitSet([0])'
+    assert list(map(list, bitset)) == [[False] * 5]
     assert bitset.add(a)
-
-
-def test_mixture_set():
-    mixset = collect.MixtureSet(0.1)
-    assert not mixset
-    assert mixset.add(np.array([0.2, 0.1, 0.7]))
-    assert not mixset.add(np.array([0.25, 0.05, 0.7]))
-    assert mixset.add(np.array([0.3, 0, 0.7]))
-    assert len(mixset) == 2
-    mixset.clear()
-    assert repr(mixset) == 'MixtureSet([])'
-    assert mixset.add(np.array([0.2, 0.1, 0.7]))
